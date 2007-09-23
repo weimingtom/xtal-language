73822a599ab7987e1a57ba966dd76e8583a6c0d7
dofixx <dofixx@2a538ec3-9d2e-0410-833a-dbe6a5d41180>
2007-09-23 04:46:13 +0000

    エラー周りをちょっと直した

--- xtal/xtal_codebuilder.h
+++ xtal/xtal_codebuilder.h
@@ -21,7 +21,7 @@ public:
 	/**
 	* コンパイルエラーを取得する。
 	*/
-	ArrayPtr errors();
+	AnyPtr errors();
 
 private:
 
@@ -39,47 +39,9 @@ private:
 
 	void compile_expr(const AnyPtr& p, const CompileInfo& info = CompileInfo());
 	void compile_stmt(const AnyPtr& p);	
-	
-	int_t register_identifier(const InternedStringPtr& v){
-		if(const AnyPtr& pos = identifier_map_->at(v)){ return pos->to_i(); }
-		result_->identifier_table_->push_back(v);
-		identifier_map_->set_at(v, result_->identifier_table_->size()-1);
-		return result_->identifier_table_->size()-1;
-	}
 
-	int_t register_value(const AnyPtr& v){
-		if(const AnyPtr& pos = value_map_->at(v)){ return pos->to_i(); }
-		result_->value_table_->push_back(v);
-		value_map_->set_at(v, result_->value_table_->size()-1);
-		return result_->value_table_->size()-1;
-	}
-
-	int_t append_identifier(const InternedStringPtr& identifier){
-		result_->identifier_table_->push_back(identifier);
-		return result_->identifier_table_->size()-1;
-	}
-
-	int_t append_value(const AnyPtr& v){
-		result_->value_table_->push_back(v);
-		return result_->value_table_->size()-1;
-	}
-	
-	MapPtr value_map_;
-	MapPtr identifier_map_;
-		
-	/**
-	* ラベル番号を予約し、返す。
-	*/
 	int_t reserve_label();
-	
-	/**
-	* ラベルを設定する。
-	*/
 	void set_label(int_t lableno);
-
-	/**
-	* ラベルにジャンプするコードを埋め込めるように細工する。
-	*/	
 	void set_jump(int_t offset, int_t labelno);
 	void process_labels();
 
@@ -90,23 +52,10 @@ private:
 	void put_set_send_code(const InternedStringPtr& var, ExprPtr pvar, bool if_defined);
 	void put_member_code(const InternedStringPtr& var, ExprPtr pvar, bool if_defined);
 	void put_define_member_code(const InternedStringPtr& var, ExprPtr pvar);
-
-	/**
-	* 識別子が変数としてあるか探し、変数位置を返す。
-	* @param key 識別子
-	* @retval -1 登録されていない
-	* @retval 非-1 変数位置
-	*/
 	int_t lookup_instance_variable(const InternedStringPtr& key);
 	void put_set_instance_variable_code(const InternedStringPtr& var);
 	void put_instance_variable_code(const InternedStringPtr& var);
 	void put_if_code(ExprPtr cond, int_t label_if, int_t label_if2);
-	void push_loop(int break_labelno, int continue_labelno, const InternedStringPtr& name = 0, bool have_label = false);
-	void pop_loop();
-	
-	/**
-	* ブロックの終りを埋め込む
-	*/
 	void break_off(int_t to);
 	
 	void compile_bin(const ExprPtr& e);
@@ -115,8 +64,8 @@ private:
 	void compile_incdec(const ExprPtr& e);
 	void compile_loop_control_statement(const ExprPtr& e);
 	void compile_class(const ExprPtr& e);
-	void compile_block(const ExprPtr& e);
 	void compile_fun(const ExprPtr& e);
+	void compile_for(const ExprPtr& e);
 
 	void put_inst2(const Inst& t, uint_t sz);
 
@@ -125,14 +74,8 @@ private:
 		put_inst2(t, sizeof(t));
 	}
 	
-	/**
-	* コードのサイズを得る。
-	*/
 	int_t code_size();
-		
-	void assign_lhs(ExprPtr lhs);
-	void define_lhs(ExprPtr lhs);
-	
+			
 	struct FunFrame{
 
 		int_t var_frame_count;
@@ -206,106 +149,19 @@ private:
 		int_t class_core_num;
 	};
 
-	void scope_chain(int_t var_frame_size){
-		for(int_t i=0; i<var_frame_size; ++i){
-			var_frames_[i].scope_chain = true;
-		}
-	}
-
 	struct LVarInfo{
 		VarFrame* var_frame;
 		int_t pos;
 	};
 
-	LVarInfo var_find(const InternedStringPtr& key, bool define = false){
-		LVarInfo ret = {0, 0};
-		for(size_t i = 0, last = var_frames_.size(); i<last; ++i){
-			VarFrame& vf = var_frames_[i];
-			for(size_t j = 0, jlast = vf.entrys.size(); j<jlast; ++j){
-				if(raweq(vf.entrys[vf.entrys.size()-1-j].name, key)){
-					if(define){ vf.entrys[vf.entrys.size()-1-j].initialized = true; }
-					if(vf.fun_frames_size!=fun_frames_.size() || vf.entrys[vf.entrys.size()-1-j].initialized){
-						ret.var_frame = &vf;
-						scope_chain(i);
-						return ret;
-					}
-				}
-				ret.pos++;
-			}
-		}
-		ret.pos = -1;
-		return ret;
-	}
-
-	void var_begin(int_t kind, int_t addsz=0){
-		VarFrame& vf = var_frames_.push();
-		vf.entrys.clear();
-		vf.directs.clear();
-		vf.block_core_num = 0;
-		vf.kind = kind;
-		vf.fun_frames_size = fun_frames_.size()+addsz;
-		vf.scope_chain = false;
-	}
-
-	void var_define(const ArrayPtr& stmts){
-		Xfor(v0, stmts->each()){
-			if(!v0){
-				continue;
-			}
-
-			ExprPtr v = ep(v0);
-			if(ep(v)->type()==EXPR_DEFINE){
-				if(v->bin_lhs()->type()==EXPR_LVAR){
-					var_define(v->bin_lhs()->lvar_name());
-				}
-			}else if(v->type()==EXPR_MASSIGN){
-				if(v->massign_define()){
-					Xfor(v1, v->massign_lhs_exprs()){
-						ExprPtr vv = ep(v1);
-						if(vv->type()==EXPR_LVAR){
-							var_define(vv->lvar_name());
-						}			
-					}
-				}
-			}
-		}
-	}
-
-	void var_define(const InternedStringPtr& name, int_t accessibility = 0, bool define = false){
-		VarFrame& vf = var_frames_.top();
-		VarFrame::Entry entry;
-		entry.name = name;
-		entry.constant = false;
-		entry.initialized = define;
-		entry.accessibility = accessibility;
-		vf.entrys.push_back(entry);
-	}
-
-	void var_set_direct(VarFrame& vf){
-		VarFrame::Direct d;
-		d.pos = code_size();
-		vf.directs.push_back(d);
-	}
-
-	void var_set_on_heap(int_t i=0){
-		if(i<var_frames_.size())
-		//for(uint_t i=0; i<var_frames_.size(); ++i){
-			if(var_frames_[i].kind==VarFrame::SCOPE){
-				var_frames_[i].kind = VarFrame::BLOCK;
-			}
-		//}
-	}
-
-	void var_end(){
-		VarFrame& vf = var_frames_.top();
-		if(vf.kind==VarFrame::SCOPE){
-			for(uint_t i=0; i<vf.directs.size(); ++i){
-				Inst* p = (Inst*)&result_->code_[vf.directs[i].pos];
-				p->op += 1;
-			}
-		}
-		var_frames_.pop();
-	}
+	void scope_chain(int_t var_frame_size);
+	LVarInfo var_find(const InternedStringPtr& key, bool define = false);
+	void var_begin(int_t kind);
+	void var_define(const ArrayPtr& stmts);
+	void var_define(const InternedStringPtr& name, int_t accessibility = 0, bool define = false);
+	void var_set_direct(VarFrame& vf);
+	void var_set_on_heap(int_t i=0);
+	void var_end();
 
 	void block_begin();
 	void block_end();
@@ -316,7 +172,34 @@ private:
 		return class_frames_.empty() ? 0 : class_frames_.top().class_core_num;
 	}
 
+	int_t register_identifier(const InternedStringPtr& v){
+		if(const AnyPtr& pos = identifier_map_->at(v)){ return pos->to_i(); }
+		result_->identifier_table_->push_back(v);
+		identifier_map_->set_at(v, result_->identifier_table_->size()-1);
+		return result_->identifier_table_->size()-1;
+	}
+
+	int_t register_value(const AnyPtr& v){
+		if(const AnyPtr& pos = value_map_->at(v)){ return pos->to_i(); }
+		result_->value_table_->push_back(v);
+		value_map_->set_at(v, result_->value_table_->size()-1);
+		return result_->value_table_->size()-1;
+	}
+
+	int_t append_identifier(const InternedStringPtr& identifier){
+		result_->identifier_table_->push_back(identifier);
+		return result_->identifier_table_->size()-1;
+	}
+
+	int_t append_value(const AnyPtr& v){
+		result_->value_table_->push_back(v);
+		return result_->value_table_->size()-1;
+	}
+	
+
 private:
+	MapPtr value_map_;
+	MapPtr identifier_map_;
 
 	CompileError errorimpl_;
 	CompileError* error_;

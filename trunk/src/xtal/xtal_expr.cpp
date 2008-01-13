604fb7311f0941168726dd45159873e77604f177
dofixx <dofixx@2a538ec3-9d2e-0410-833a-dbe6a5d41180>
2008-01-13 03:53:25 +0000

    追加し忘れ

---
+++ trunk/src/xtal/xtal_expr.cpp
@@ -0,0 +1,26 @@
+#include "xtal.h"
+
+namespace xtal{
+
+const AnyPtr& Expr::at(uint_t i){
+	if(size()<=i)
+		resize(i+1);
+	return Array::at(i);
+}
+
+void Expr::set_at(uint_t i, const AnyPtr& v){
+	if(size()<=i) 
+		resize(i+1); 
+	Array::set_at(i, v); 
+}
+
+void ExprBuilder::splice(int_t tag, int_t num){
+	ExprPtr ret = xnew<Expr>(tag, lineno_);
+	for(uint_t i=root_->size()-num; i<root_->size(); ++i){
+		ret->push_back(root_->at(i));
+	}
+	root_->resize(root_->size()-num);
+	root_->push_back(ret);
+}
+
+}
